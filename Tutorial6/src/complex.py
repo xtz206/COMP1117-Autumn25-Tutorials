1dfd3a0850aa092480fbfd9889b5a1e866cb576d
xtz206 <ywx2006@outlook.com>
2025-10-30 00:01:47 +0800

    fix(tut6): format code and update extension

--- Tutorial6/src/complex.py
+++ Tutorial6/src/complex.py
@@ -1,14 +1,19 @@
 class Complex:
-    def __init__(self, real: float, imag: float):
-        self.real = real
-        self.imag = imag
+    def __init__(self, re: float, im: float):
+        self.re = re
+        self.im = im
     def __add__(self, other):
-        return Complex(self.real + other.real,
-                        self.imag + other.imag)
+        return Complex(self.re + other.re,
+                        self.im + other.im)
     def __str__(self):
-        if self.imag < 0:
-            return f"{self.real} - {-self.imag}i"
-        return f"{self.real} + {self.imag}i"
+        if self.im < 0:
+            return f"{self.re} - {-self.im}i"
+        return f"{self.re} + {self.im}i"
+    def __eq__(self, other):
+        return self.re == other.re and self.im == other.im
+    def __hash__(self):
+        return hash((self.re, self.im))
+
 c1 = Complex(2, 3)
 c2 = Complex(4, 5)
 c3 = c1 + c2
